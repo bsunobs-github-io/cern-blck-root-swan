2e6a9418ca175909cc4980a17e96802745d9c316
Vassil Vassilev <v.g.vassilev@gmail.com>
2021-09-11 18:12:30 +0000

    llvm::StringRef does not support automatic conversion to string anymore.
    See llvm/llvm-project@777180a32b61

--- core/dictgen/src/LinkdefReader.cxx
+++ core/dictgen/src/LinkdefReader.cxx
@@ -770,7 +770,7 @@ public:
       } else {
          llvm::StringRef include(start, fSourceManager.getCharacterData(end.getLocation()) - start + end.getLength());
 
-         if (!fOwner.AddInclude(include)) {
+         if (!fOwner.AddInclude(include.str())) {
             Error("", tok);
          }
       }
@@ -928,7 +928,7 @@ public:
       } else {
          llvm::StringRef identifier(start, fSourceManager.getCharacterData(end.getLocation()) - start + end.getLength());
 
-         if (!fOwner.AddRule(type, identifier, linkOn, false, options.get())) {
+         if (!fOwner.AddRule(type.str(), identifier.str(), linkOn, false, options.get())) {
             Error(type.data(), tok, false);
          }
       }
@@ -987,7 +987,7 @@ public:
 
       llvm::StringRef identifier(start, fSourceManager.getCharacterData(end.getLocation()) - start + end.getLength());
 
-      if (!fOwner.AddRule("class", identifier, true, true)) {
+      if (!fOwner.AddRule("class", identifier.str(), true, true)) {
          Error("", tok);
       }
 
